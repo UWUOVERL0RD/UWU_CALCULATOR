26af52bb60dd396a5d66334afb9222e5d5e7335b
UWU OVERLORD <160476201+UWUOVERL0RD@users.noreply.github.com>
2024-02-19 10:28:31 +0000

    ADD THE ACTUALL CALCULATOR INTO THE GIT HUB REPOSITORY

---
+++ UWU_CALCULATOR.py
@@ -0,0 +1,444 @@
+#!/usr/bin/python3
+
+# UWU CALCULATOR
+# CREATED
+#         BY
+#            THE
+#                UWU
+#                    OVERLORD
+#                             IN
+#                                THE
+#                                    YEAR
+#                                         OF:
+#                                           :
+#                                           : 20 TWENTY
+#                                           : 20 TWENTY
+#                                           :  4   FOUR
+
+# P L E A S E
+# * S T A R *
+# T    H    E -------------------------------------v
+#                                                GITHUB
+#                                                   REPOSITORY
+#                                       GITHUB DOT COM SLASH UWU OVERLORD (SECOND LETTER O IS A ZERO FOR NO REASON) SLASH UWU CALCULATOR
+
+# PROGRAM DESCRIPTION    : THIS IS A CALCULATOR
+#         FUNCTION       : CALCULATE STUFF
+#         VULNERABILITIES: ALL OF THEM
+#         LANGUAGE       : ENGLISH
+#         AUTHOR         : UWU OVERLORD
+
+# IF YOU READ THIS THEN
+#                   THEN
+#                    THEN
+#                     THEN
+#                      THEN
+#                     THEN
+#                    THEN
+#                   THEN
+#                  THEN
+#                   THEN
+#                    THEN
+#                     THEN
+#                      THEN
+#                     THEN
+#                    THEN
+#                   THEN
+#                  THEN
+#                   THEN
+#                    THEN
+#                     THEN
+#                      THEN
+#                     THEN
+#                    THEN
+#                   THEN
+#                  THEN
+#                   THEN
+#                    THEN
+#                     THEN
+#                      THEN
+#                     THEN
+#                    THEN
+#                   THEN
+#                  THEN
+#                   THEN
+#                    THEN
+#                     THEN
+#                      THEN
+#                     THEN
+#                    THEN
+#                   THEN
+#                  THEN
+#                   THEN
+#                    THEN
+#                     THEN
+#                      THEN
+#                       T HEN
+#                       T H EN
+#                       T H E N
+#                       T  H E N
+#                       T  H  E N
+#                       T   H  E  N
+#                       T   H   E   N
+#                                        LLLL
+#                                        MMMM
+#                                        AAAA
+#                                        OOOO
+#                           I JUST WASTED YOUR T
+#                                             I
+#                                              ME
+
+
+# ANYWAY!!!!
+# BELOW
+# VVVVV
+#  I S
+# VVVVV
+#  THE
+# VVVVV
+# P
+#  R
+#   O
+#    G
+#     R
+#    A
+#   M
+# !
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# OKAY
+#      SO
+#         STEP
+#                1                  1
+#               1   O     N     E    1
+#                1                  1
+import sys
+# ^^^^^^^^^^^^^^^
+# |||||||||||||||
+# THIS
+#   IS R E Q U I R E D
+#                    | 4
+#                    V
+#             EXIT F()UNCTIONALITY
+
+
+# AND NOW
+#       ENTER
+#         H
+#         E
+
+#     M     M    A     IIIII N   N
+#     MM   MM   A A      I   NN  N
+#     M M M M  A   A     I   N N N
+#     M  M  M  AAAAA     I   N  NN
+#     M     M  A   A   IIIII N   N
+#           L   O   O   P            <----------------|  THE WORD << LOOP >> IS OVER THERE ON THE <- L E F T <-
+#                                                     |
+# I DECIDED TO NOT MAKE F                             |
+#                        F                            |
+#                         F                           |
+#                          F A N C Y                  |
+#                                   ART FOR THE WORD -|
+#                                                       BECAUSE IT TAKES SOOOOOO
+#                                                                              O
+#                                                                              O
+#                    YES                                                       O
+#                        IT                                                    O
+#                           TAKES                                              O
+#                                 THIS                                         O
+#                                      MUCH                                    O
+#                                           TIME                               O
+#                                        TO                                    O
+#                                   MAKE                                       O
+#                             THESE                                            O
+#                      THINGS                                                  O
+#                 WITH                                                         O
+#                                         LETTERSSSSSSSSSSSSSSSS               O
+#                                                                              O
+#                                                                              O
+#                     ^                                                        O
+#                     |- WINDOWS COMMAND LINE PROMPT???                        O
+#                                                                              O
+#                                                                                 MUCH TIME! ! ! !
+
+while True:
+    print("WELCOME TO UWU CALCULATOR") # TELL THEM THE > NAME < (WHAT IF THEY ?
+                                       #                        )              F
+                                       #                        (             O
+                                       #                        )              R
+                                       #                        (             G
+                                       #                        )              O
+                                       #                        (             T
+                                       #                        )              ?
+                                       #                        (
+                                       #                        )
+    print("BY THE UWU OVERLORD") # SELF /!\ A D V E R T I S E M E N T /!\
+    print("TYPE AN EXPRESSION LIKE 2+2") # EXPRESSION SUGGESTION (THAT RHYMES!!!)
+                                         #                             RHYMES!!
+                                         #                             RHYMES!
+                                         #                             RHYMES
+                                         #                             RHYME
+                                         #                             RHYM
+                                         #                             RHY
+                                         #                             RH
+                                         #                             R
+                                         #                            HR
+                                         #                           YHR
+                                         #                          MYHR
+                                         #                         EMYHR
+                                         #                        SEMYHR
+                                         #                       !SEMYHR
+                                         #                      !!SEMYHR
+                                         #                     !!!SEMYHR
+    THE_EXPRESSION = input(">") # TAKE THE INPUT FROM THE USER
+
+    # NOW
+    #  IS
+    # THE
+    # IMP
+    #  OR
+    #   T
+    #  AN
+    #   T
+    # PAR
+    #   T
+    # !!!
+    
+    #                 _             _             _
+    # WE             /.\           /.\           /.\
+    #               /. .\         /. .\         /. .\
+    #              /. . .\       /. . .\       /. . .\
+    #             /. . . .\     /. . . .\     /. . . .\
+    #            /. . . . .\   /. . . . .\   /. . . . .\
+    #           /. . . . . .\ /. . . . . .\ /. . . . . .\
+    #          /. . . . . . .V. . . . . . .V. . . . . . .\
+    #         /. . . . . . . . . . . . . . . . . . . . . .\
+    #        /. . . . . . . . . . . . . . . . . . . . . . .\
+    #        E   V   A   L   U   A   T   E     I   T   !   !
+
+    # ! GOOD !
+    # NOWW
+    #    WE
+    #       PRINTT
+    #            THE
+    #                VA
+    #                 LU
+    #                  EEEEEEE
+    #                  E
+    #                  EEEEEE
+    #                  E
+    #                  EEEEEEE
+    #                             !!
+    #                             !!
+    #                             !!
+    #
+    #                             !!
+    # YEAH!!!
+    # BUT
+    #             WAIT!!!
+    #            W A I T ! ! !
+    #         W  A  I  T  !  !  !
+    #      W   A   I   T   !   !   !   
+    #   W    A    I    T    !    !    !
+    #   WWWWWAAAAAIIIIITTTTT!!!!!!!!!!!
+    #                                           WHAT
+    #                                               IF
+    #                                                 THE
+    #                                                     USER
+    #                                                          TYPES
+    #                                               SOMETHING LIKE...
+    #                                                             ....
+    #                                                             .....
+    #                                                            ......
+    #                                                           .......          <---- OH LOOK I MADE A BANANA!
+    #                                                          ........                                  ANANA!
+    #                                                         ........
+    #                                                      ........
+    #   SOMETHING LIKE: 2OJFWEIFJ2OIJF23JF2JEF2JR920FJ29FI3FJI2I3FJ3FFJ923JR0283JF2UR2CR8U2980CU09MCF90U1C298VRM93FCUM34UXF9IWRJEI
+    #                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    #                   THIS IS NOT A VALID PYTHON EXPRESSION BECAUSE IT IS JUST A JUMBLE OF COMPLETELY RANDOM NUMBERS AND LETTERS
+    #   WE HAVE A PROBLEM.
+    #   ??? HOW    ???       !!! WELL     !!!
+    #   ??? DO     ???       !!! THE      !!!
+    #   ??? WE     ???       !!! SOLUTION !!!
+    #   ??? HANDLE ???       !!! IS       !!!
+    #   ??? THE    ???       !!! TRY      !!!
+    #   ??? ERROR  ???       !!! CATCH    !!!
+    #                                         YOU                : TRY CATCH???
+    #                                         UWU OVERLORD AKA ME: TRY CATCH!!!
+    #                                         YOU                : WHAT IS THAT?
+    #                                         UWU OVERLORD AKA ME: WHAT DO YOU MEAN BY """""""THAT"""""""
+    #                                         YOU                : I OF COURSE MEAN WHAT IS TRY CATCH?
+    #                                         UWU OVERLORD AKA ME: OHHHHHH I GET IT NOW
+    #     TRY CATCH IS A VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #        YUP         VERY
+    #     IT'S           VERY
+    #        THAT        VERY
+    #     COOL           VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                    VERY
+    #                         COOL FEATURE OF PYTHON
+    #                                                WE _______TRY_______ TO DO SOMETHING
+    #                                                                              ^
+    #                                                                              |
+    #                                                                            IT ERROR!
+    #                                                                                  WE ______________CATCH_______________
+    #                                                                                          THE               ERROR
+    #                                                                                               AND THEN
+    #                                                                                             WE TELL THE USER THE FOLLOWING. (PLEASE RETURN TO THE
+    #                                                                                                                              LEFT SIDE OF THE SCREEN...)
+    #  OKAY GOOD YOU'RE HERE NOW
+    #  ANYWA
+    #  Y
+    #  WE TELL THE USER SOMETHING LIKE:
+    #   "WTF BRO??? WHAT DID U JUST ENTER???"
+    #  ALSO
+    #  WE WILL ALLOW THEM TO EXIT
+    #             BY TYPING "EXIT"
+    #  NICE
+
+
+
+
+
+
+
+
+    # SO HERE IS ~O~U~R~ MY!!! PLAN
+    # 0. (THIS IS 0 BECAUSE IT WAS ADDED LATER) CHECK IF IT'S "EXIT"
+    #  0.1 IF YEAH THE BYE BAI
+    # 1. TRY
+    #  1.1 EAVL
+    #      UH NO
+    #      EVAL
+    #      ^
+    #      | THAT IS THE CORRECT SPELLING, "EVAL". NOT "EAVL"
+    #  1.2. PRINT THE VALUE
+    # 2. CATCH
+    #  2.1. CALL USER A BAKA
+    # 3. PROIT!!!!!!!
+
+    # SO LET'S DO IT
+
+    if THE_EXPRESSION.strip().lower() == "exit":
+        print("BAI BAH")
+        sys.exit()
+
+    try:
+        VALUE = eval(THE_EXPRESSION)
+        print(VALUE)
+    except:
+        # YEAH SORRY IT'S ACTUALLY CALLED TRY EXCEPT
+        #                         AND NOT TRY CATCH
+        #                                                 I AM NOT SORRY
+        print("USER WHAT THE HELL ARE YOU DOING")
+        print(f"I DID NOT TELL YOU TO EVER TYPE \"\"{THE_EXPRESSION}\"\"")
+        print("IF YOU WANTED TO EXIT JUST TYPE EXIT")
+        print()
+        print()
+    print()
+    print()
+
+
+    # AND THAT IS LITERALLY THE ENTIRE UWU OVERLORD'S PERSONAL OPEN SOUORCE SOFTWARE GPL'ED CALCULATOR
+    # THANK YOU
+    # IF YOU READ EVERYTHING THEN YOU ARE ONE OF THE FOLLOWING:
+    #  (I USED AN ASCII ART GENERATOR INSTEAD OF MAKING THIS MYSELF
+    #   ALL CREDIT IS TO THE WEBSITE CALLED PATORJK DOT COM SLASH SOFTWARE
+    #   SHASH TAAG)
+    #  (BUT IT LOOKS COOL SO WHATEVER)
+    #                 1.  _____    _ _ _    _____    _____    _____    _____    _____ 
+    #                    |  _  |  | | | |  |   __|  |   __|  |     |  |     |  |   __|
+    #                    |     |  | | | |  |   __|  |__   |  |  |  |  | | | |  |   __|
+    #                    |__|__|  |_____|  |_____|  |_____|  |_____|  |_|_|_|  |_____|
+    # 2.    .--.     .-. . .-.   .----.    .----.    .----.    .-.   .-.   .----.
+    #      / {} \    | |/ \| |   | {_     { {__     /  {}  \   |  `.'  |   | {_  
+    #     /  /\  \   |  .'.  |   | {__    .-._} }   \      /   | |\ /| |   | {__ 
+    #     `-'  `-'   `-'   `-'   `----'   `----'     `----'    `-' ` `-'   `----'
+    # (THE FOLLOWING A W E S O M E TEXTS WILL BE ON THE 0 INDENTATION LEVEL BECAUSE OF TECHNICAL DIFFICULTIES(TM). WE (I) HOPE YOU UNDERSTAND.)
+
+
+
+
+
+
+
+
+
+
+
+# 3:
+#  _______       ___       __   __________   ________    _______     ______  ___   __________
+#  ___    |      __ |     / /   ___  ____/   __  ___/    __  __ \    ___   |/  /   ___  ____/
+#  __  /| |      __ | /| / /    __  __/      _____ \     _  / / /    __  /|_/ /    __  __/
+#  _  ___ |      __ |/ |/ /     _  /___      ____/ /     / /_/ /     _  /  / /     _  /___
+#  /_/  |_|      ____/|__/      /_____/      /____/      \____/      /_/  /_/      /_____/
+
+# 4:
+#     ___   _      __  ____  ____  ____    __  ___  ____
+#    / _ | | | /| / / / __/ / __/ / __ \  /  |/  / / __/
+#   / __ | | |/ |/ / / _/  _\ \  / /_/ / / /|_/ / / _/
+#  /_/ |_| |__/|__/ /___/ /___/  \____/ /_/  /_/ /___/
+#
+
+# TOO LAZY TO ADD MORE AWESOMES
